7ea7fa7d4a1549e9605cbe537bc04b400b0f51cf
Ravi Kumar <ravikumargrk@hotmail.com>
2024-03-12 21:09:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
-parser = reqparse.RequestParser()
-parser.add_argument('code', type=str)
-parser.add_argument('input', type=str)
 
 import os 
+USER = os.environ.get('USER')
+PASS = os.environ.get('PASS')
+
 def run(code, stdin):
     f = open("foo.py", "w")
     f.write(code)
@@ -24,16 +24,27 @@ def run(code, stdin):
     f.close()
     return stdout
 
+def check(body):
+    keys = ['code', 'username', 'password', 'input']
+    for key in keys:
+        if key not in body:
+            return False
+    return True
+
 class root(Resource):
     def get(self):
         return {'documentation': 'Post {"code":"", "input":""} and receive {"output":""}, the output of the given code with given input'}
     def post(self):
-        args = parser.parse_args()
-        code = args['code']
-        stdin = args['input']
-        return {"output": run(code, stdin)}
+        args = request.get_json()
+        if check(args):
+            if ((USER == args['username']) and (PASS == args['password'])):
+                return run(args['code'], args['stdin'])
+            else:
+                return {"log": "invalid auth"}
+        else:
+            return {"log":"invalid payload"}
 
 api.add_resource(root, '/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
